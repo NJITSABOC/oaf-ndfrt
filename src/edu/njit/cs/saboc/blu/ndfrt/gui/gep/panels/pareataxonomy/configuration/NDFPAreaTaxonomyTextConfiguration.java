375b5bd1cdeb064a7a98ae5af281bd3c2028ce92
Christopher Ochs <cro3@njit.edu>
2017-08-26 19:52:13 -0400

    Refactored text configurations

--- src/edu/njit/cs/saboc/blu/ndfrt/gui/gep/panels/pareataxonomy/configuration/NDFPAreaTaxonomyTextConfiguration.java
+++ src/edu/njit/cs/saboc/blu/ndfrt/gui/gep/panels/pareataxonomy/configuration/NDFPAreaTaxonomyTextConfiguration.java
@@ -1,7 +1,5 @@
 package edu.njit.cs.saboc.blu.ndfrt.gui.gep.panels.pareataxonomy.configuration;
 
-import edu.njit.cs.saboc.blu.core.abn.pareataxonomy.Area;
-import edu.njit.cs.saboc.blu.core.abn.pareataxonomy.PArea;
 import edu.njit.cs.saboc.blu.core.abn.pareataxonomy.PAreaTaxonomy;
 import edu.njit.cs.saboc.blu.core.gui.gep.panels.details.pareataxonomy.configuration.PAreaTaxonomyTextConfiguration;
 import edu.njit.cs.saboc.blu.ndfrt.gui.gep.configuration.NDFRTEntityNameConfiguration;
@@ -14,15 +12,5 @@ public class NDFPAreaTaxonomyTextConfiguration extends PAreaTaxonomyTextConfigur
     
     public NDFPAreaTaxonomyTextConfiguration(PAreaTaxonomy taxonomy) {
         super(new NDFRTEntityNameConfiguration(), taxonomy);
-    }
-
-    @Override
-    public String getNodeHelpDescription(PArea parea) {
-        return "*** PAREA HELP TEXT ***";
-    }
-    
-    @Override
-    public String getContainerHelpDescription(Area container) {
-        return "*** AREA HELP TEXT ***";
     }
 }
